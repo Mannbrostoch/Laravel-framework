89500f5369f4d2e7117c86659ca53e2fa90f8707
Mann brostoch <manbrostoch@gmail.com>
2024-03-12 15:42:48 +0700

    add taiwindcss

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,12 @@
-<h1>About Page</h1>
-<p>This is the about page.</p></h1>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1> 
+</body>
+</html>
